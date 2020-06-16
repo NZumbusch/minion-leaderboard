bdd8ac0915b124ccff04c8baff076e3eb217ec67
Lucifer <lucifermonao@gmx.de>
2020-06-16 11:24:38 +0200

    Fixed _SESSION var not working as supposed, changed settings method to post

--- includes/change_settings.inc.php
+++ includes/change_settings.inc.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <?php
 
-if (!isset($_GET["submit"])) {
+session_start();
+
+if (!isset($_POST["submit"])) {
     header("LOCATION: ../index.php");
     exit();
 }
 
-if (isset($_GET["amount"])) {
-    $_SESSION["amount"] = $_GET["amount"];
+if (isset($_POST["amount"])) {
+    $_SESSION["amount"] = $_POST["amount"];
 }
 
-if (isset($_GET["level"])) {
-    $_SESSION["level"] = $_GET["level"];
+if (isset($_POST["level"])) {
+    $_SESSION["level"] = $_POST["level"];
 }
 
-if (isset($_GET["fuel"])) {
-    $_SESSION["fuel"] = $_GET["fuel"];
+if (isset($_POST["fuel"])) {
+    $_SESSION["fuel"] = $_POST["fuel"];
 }
 
-if (isset($_GET["afk"])) {
-    $_SESSION["afk"] = $_GET["afk"];
+if (isset($$_POST["afk"])) {
+    $_SESSION["afk"] = $_POST["afk"];
 }
 
-if (isset($_GET["diamondspreading"])) {
-    if ($_GET["diamondspreading"] == "on") {
+if (isset($_POST["diamondspreading"])) {
+    if ($_POST["diamondspreading"] == "on") {
         $_SESSION["diamondspreading"] = "true";
     } else {
         $_SESSION["diamondspreading"] = "false";
