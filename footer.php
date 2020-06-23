4cab9286bb265e069bf19df3507c8f4da482295f
Lucifer <lucifermonao@gmx.de>
2020-06-23 10:57:51 +0200

    Added footer, new Styles & Images

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="footer">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles/footer.css">
+    </head>
+    <body>
+        <div class="footer">
+
+            <a class="Github" href="https://github.com/LuciferMonao/minion-leaderboard"><img src="src/img/github.png" alt="Github"></a>
+            <a class="Discord" href="https://discord.com"><img src="src/img/discord.png" alt="Discord"></a>
+            <a class="Mail" href="mailto:Lucifermonao@gmx.com"><img src="https://img.icons8.com/windows/32/000000/email-open.png"/></a>
+            <p>&copy 2020, <a href= "mailto:Lucifermonao@gmx.com">Lucifer Monao</a>, Some Rights Reserved</p>
+        </div>
+    </body>
+</html>
