73d90a7b8c0c64f844bff872daca4858081c9f2a
Lucifer <lucifermonao@gmx.de>
2020-06-24 15:12:29 +0200

    Part of last commit...

---
+++ header.php
@@ -0,0 +1,26 @@
+<nav class="nav">
+<link rel="stylesheet" href="styles/header.css">
+
+    <div class="container">
+        <a class="home" id="about-btn" href="index.php"><img src="src/img/Hypixel.jpeg" alt="Hypixel Logo"></a>
+        <p>Minion Leaderboard for Hypixel Skyblock</p>
+        <a class="settings-link" href="settings.php"><img src="https://img.icons8.com/carbon-copy/100/000000/gear.png"/></a>
+    </div>
+            
+    <div class="container2"></div>
+        <div id="about" class="hidden">
+            <h1>This website was created by Lucifer Monao.</h1>
+            <h2>Contact: </h2>
+            <h2>&nbsp&nbsp&nbspEmail: <a class="Maillink" href= "mailto:Lucifermonao@gmx.com">Lucifermonao@gmx.de</a></h2>
+            <h2>&nbsp&nbsp&nbspDiscord: Creawana#9093</h2>
+            <h2>&nbsp&nbsp&nbspIngame: Creawana</h2>
+            <h2>If you discover any bugs / have any issues, please create a new issue over on github.</h2>
+            <h2>Get there by clicking the github logo on the bottom left.</h2>
+        </div>
+
+        <form action="#" method="none" class="search" id="search" autocomplete="off">
+            <input class="text-input" type="text" name="search-entered" id="search-entered" placeholder="What minion do you want to search?">
+            <input class = "search-button" type="button" name="search-button" id="search-button" value="Search">
+        </form>
+    </div>
+</nav>
